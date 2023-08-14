03b572425d93a6a20675dc62c8f63be67d7b112f
Raki <marianaraquelvegas@gmail.com>
2023-08-13 23:11:46 -0300

    Se crea class Perfil

--- perfiles/models.py
+++ perfiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Perfil(models.Model):
+    username =models.CharField(max_length=256)
+    apellido = models.CharField(max_length=256)
+    nombre = models.CharField(max_length=256)
+    email = models.EmailField(blank=True)
+    fecha_nacimiento = models.DateField(null=True)
+
+    def __str__(self):
+        return f"{self.username}, {self.apellido}, {self.nombre}"
