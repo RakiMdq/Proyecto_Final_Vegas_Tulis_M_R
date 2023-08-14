68ee7a0df8b91d89eb3260f790f8d2adaf3f0893
Raki <marianaraquelvegas@gmail.com>
2023-08-14 15:06:43 -0300

    Creando modelos y clases

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+class Blogs(models.Model):
+    titulo= models.CharField(max_length=255)
+    slog = models.SlugField(unique=True)
+    subtitulo= models.CharField(max_length=255)
+    contenido = models.TextField()
+    
+    fecha = models.DateTimeField(auto_now_add=True)
+    autor = models.ForeignKey(User, on_delete=models.PROTECT)
+    imagen = models.ImageField(upload_to='publicaciones/')
+    
+
+    def __str__(self):
+        return self.titulo
+
+class EditarPublicacion(models.Model):
+    publicacion = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+    contenido = models.TextField(blank=True, null=True)
+    fecha_edicion = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.publicacion.titulo} - {self.fecha_edicion}'
+
+class Comentario(models.Model):
+    publicacion = models.ForeignKey(Blogs, on_delete=models.CASCADE)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    contenido = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentario de '{self.autor.username}' en '{self.publicacion.titulo}'"
--- perfiles/models.py
+++ perfiles/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 class Perfil(models.Model):
-    username =models.CharField(max_length=256)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     apellido = models.CharField(max_length=256)
     nombre = models.CharField(max_length=256)
     email = models.EmailField(blank=True)
-    fecha_nacimiento = models.DateField(null=True)
+    foto = models.ImageField(upload_to='profile_photos', blank=True)
 
     def __str__(self):
-        return f"{self.username}, {self.apellido}, {self.nombre}"
+        return self.usuario.username
+    
+class Avatar(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=1)
+    foto = models.ImageField(upload_to='avatars/', default='usuarios/img/fotodeperfil.jpg')
