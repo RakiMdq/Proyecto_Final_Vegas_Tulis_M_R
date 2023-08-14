0ecfbea51f80e4185ac77877df4a1b0dbde38bb3
Raki <marianaraquelvegas@gmail.com>
2023-08-14 16:16:30 -0300

    Subiendo publicaciones desde admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from blog.models import Blogs, EditarPublicacion, Comentario
 # Register your models here.
+
+admin.site.register(Blogs)
+admin.site.register(EditarPublicacion)
+admin.site.register(Comentario)
+
+
+
+
