a85eb206d4492b84dae3ee2430dae1cc9e9ceb12
Raki <marianaraquelvegas@gmail.com>
2023-08-14 22:17:05 -0300

    Agragando hmtl/statis/etc

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, include
+from blog.views import listar_publicaciones, detallar_publicacion
 
 urlpatterns = [
-    
+
+    path("publicaciones/", listar_publicaciones),
+    path("detalle/", detallar_publicacion),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from blog.models import Blogs, EditarPublicacion, Comentario
 
 # Create your views here.
 
+def listar_publicaciones(request):
+    publicaciones = Blogs.objects.all()
+    if publicaciones:
+        context = {'publicaciones': Blogs}
+    else:
+        mensaje = 'No hay publicaciones creadas.'
+        context = {'mensaje': mensaje}
+    return render(request, 'blog/lista_publicaciones', context)
 
+def detallar_publicacion(request, pk):
+    publicacion = get_object_or_404(Blogs, pk=pk)
+    creador = publicacion.creador
+    return render(request, 'blog/detalle_publicaciones', {'publicacion': publicacion, 'creador': creador})
